b6839923e7f78895652a665df5c5ac3ed0b8cd68
RECTOR <rheza10@gmail.com>
2025-12-13 01:52:05 +0700

    fix: Migrate to Kotlin compilerOptions DSL
    Replace deprecated kotlinOptions.jvmTarget with compilerOptions DSL
    to fix build failure with Kotlin 2.0+.
    
    Fixes CI build failure.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,9 @@ tasks {
     }
 
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     patchPluginXml {
